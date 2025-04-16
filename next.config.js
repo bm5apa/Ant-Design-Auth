be73ac466d9f0f9d4fcfc9dffb727f61fe48d8f7
Melissa Lu <timelessheaven@gmail.com>
2025-04-16 16:36:20 +0800

    fix: fix login issue

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-  /* config options here */
-=======
   reactStrictMode: true,
   swcMinify: true,
->>>>>>> 013a2ff
 };
 
 module.exports = nextConfig;
