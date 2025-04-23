2ed32921a1a6e51938a75d2039661b62f5d93db3
Melissa Lu <timelessheaven@gmail.com>
2025-04-23 11:25:40 +0800

    fix: fix sign-up function

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,15 +1,38 @@
 import { NextResponse } from "next/server";
 import { users } from "../data/data";
+import bcrypt from "bcrypt";
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     const { username, password } = body;
+
     if (!username || !password) {
       return NextResponse.json(
         {
           success: false,
           message: "Username and password are required",
+        },
+        { status: 400 }
+      );
+    }
+
+    const alphabetCount = (username.match(/[a-zA-Z]/g) || []).length;
+    const numberCount = (username.match(/[0-9]/g) || []).length;
+    const passwordAlphabetCount = (password.match(/[a-zA-Z]/g) || []).length;
+    const passwordNumberCount = (password.match(/[0-9]/g) || []).length;
+
+    if (
+      alphabetCount < 4 ||
+      numberCount < 4 ||
+      passwordAlphabetCount < 4 ||
+      passwordNumberCount < 4
+    ) {
+      return NextResponse.json(
+        {
+          success: false,
+          message:
+            "Username and password must contain at least 4 letters and 4 numbers",
         },
         { status: 400 }
       );
@@ -26,8 +49,15 @@ export async function POST(request: Request) {
       );
     }
 
-    const newUser = { username, password };
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = {
+      username,
+      password: hashedPassword,
+      resetCode: null,
+      resetCodeExpires: null,
+    };
     users.push(newUser);
+
     const token = Buffer.from(`${username}:${Date.now()}`).toString("base64");
 
     return NextResponse.json({
